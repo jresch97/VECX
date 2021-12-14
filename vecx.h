4056de670ad6ba1d1c26cc6ac3c1a850dab22361
Jared B. Resch <jared.resch@hotmail.com>
2021-12-14 17:44:35 -0500

    Added interpolation and compressed files.

--- vecx.h
+++ vecx.h
@@ -30,70 +30,54 @@ typedef T NAME_LOWER[N]; \
 \
 VECX_EXPORT void NAME_LOWER ## _init(NAME_LOWER vec, T val) \
 { \
-        for (int i = 0; i < N; i++) { \
-                vec[i] = val; \
-        } \
+        for (int i = 0; i < N; i++) vec[i] = val; \
 } \
 \
 VECX_EXPORT void NAME_LOWER ## _add(NAME_LOWER a, NAME_LOWER b, NAME_LOWER out) \
 { \
-        for (int i = 0; i < N; i++) { \
-                out[i] = a[i] + b[i]; \
-        } \
+        for (int i = 0; i < N; i++) out[i] = a[i] + b[i]; \
 } \
 \
 VECX_EXPORT void NAME_LOWER ## _sub(NAME_LOWER a, NAME_LOWER b, NAME_LOWER out) \
 { \
-        for (int i = 0; i < N; i++) { \
-                out[i] = a[i] - b[i]; \
-        } \
+        for (int i = 0; i < N; i++) out[i] = a[i] - b[i]; \
 } \
 \
 VECX_EXPORT SCALAR_T NAME_LOWER ## _dot(NAME_LOWER a, NAME_LOWER b) \
 { \
         SCALAR_T dot = (T)0; \
-        \
-        for (int i = 0; i < N; i++) { \
-                dot = a[i] * b[i]; \
-        } \
-        \
+        for (int i = 0; i < N; i++) dot = a[i] * b[i]; \
         return dot; \
 } \
 \
 VECX_EXPORT SCALAR_T NAME_LOWER ## _len(NAME_LOWER vec) \
 { \
         SCALAR_T sq_sum = (T)0; \
-        \
-        for (int i = 0; i < N; i++) { \
-                sq_sum += vec[i] * vec[i]; \
-        } \
-        \
+        for (int i = 0; i < N; i++) sq_sum += vec[i] * vec[i]; \
         return SQRT(sq_sum); \
 } \
 \
 VECX_EXPORT void NAME_LOWER ## _norm(NAME_LOWER vec, NAME_LOWER out) \
 { \
         SCALAR_T len = NAME_LOWER ## _len(vec); \
-        \
-        if (len == ((SCALAR_T)0)) { \
-                return; \
-        } \
-        \
-        for (int i = 0; i < N; i++) { \
-                out[i] = vec[i] / len; \
-        } \
+        if (len == ((SCALAR_T)0)) return; \
+        for (int i = 0; i < N; i++) out[i] = vec[i] / len; \
+} \
+\
+VECX_EXPORT void NAME_LOWER ## _lerp(const NAME_LOWER a, \
+                                     const NAME_LOWER b, \
+                                     T t, \
+                                     NAME_LOWER out) \
+{ \
+        for (int i = 0; i < N; i++) out[i] = a[i] + t * (b[i] - a[i]); \
 } \
 \
 VECX_EXPORT void NAME_LOWER ## _fprintf(NAME_LOWER vec, FILE *f) \
 { \
         for (int i = 0; i < N; i++) { \
                 fprintf(f, SCALAR_FMT_STR, vec[i]); \
-                \
-                if (i < (N - 1)) { \
-                        fprintf(f, " "); \
-                } \
+                if (i < (N - 1)) fprintf(f, " "); \
         } \
-        \
         fprintf(f, "\n"); \
 } \
 \
